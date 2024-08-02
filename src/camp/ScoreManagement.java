a784430df0466ebb908503b7d12f56ab58349174
taehyunkim <98rlaxogus@gmail.com>
2024-08-02 19:56:06 +0900

    feat:inquireAverageGradeBySubject 과목별 평균점수 추가

--- src/camp/ScoreManagement.java
+++ src/camp/ScoreManagement.java
@@ -18,7 +18,8 @@ public class ScoreManagement {
             System.out.println("1. 수강생의 과목별 시험 회차 및 점수 등록");
             System.out.println("2. 수강생의 과목별 회차 점수 수정");
             System.out.println("3. 수강생의 특정 과목 회차별 등급 조회");
-            System.out.println("4. 메인 화면 이동");
+            System.out.println("4. 수강생의 특정 과목 평균 등급 조회");
+            System.out.println("5. 메인 화면 이동");
             System.out.print("관리 항목을 선택하세요...");
             int input = sc.nextInt();
 
@@ -26,7 +27,8 @@ public class ScoreManagement {
                 case 1 -> createScore(); // 수강생의 과목별 시험 회차 및 점수 등록
                 case 2 -> updateScore(); // 수강생의 과목별 회차 점수 수정
                 case 3 -> inquireRoundGradeBySubject(); // 수강생의 특정 과목 회차별 등급 조회
-                case 4 -> flag = false; // 메인 화면 이동
+                case 4 -> inquireAverageGradeBySubject();
+                case 5 -> flag = false; // 메인 화면 이동
                 default -> {
                     System.out.println("잘못된 입력입니다.\n메인 화면 이동...");
                     flag = false;
@@ -144,7 +146,6 @@ public class ScoreManagement {
                 printSubjectInfoByStudentId(studentId);
                 String inputSubjectId = sc.nextLine();
                 int indicator = Character.getNumericValue(inputSubjectId.charAt(2));
-                System.out.println(indicator);
                 //scoreStore안에있는 Map 을 지정해줌
                 Map<String, Score> innerScoreStore = scoreStore.get(studentId);
 
@@ -159,11 +160,11 @@ public class ScoreManagement {
                             System.out.println((i + 1) + "회차 등급: A");
                         } else if (num > 89) {
                             System.out.println((i + 1) + "회차 등급: B");
-                        } else if (num > 80) {
+                        } else if (num > 79) {
                             System.out.println((i + 1) + "회차 등급 : C");
-                        } else if (num > 70) {
+                        } else if (num > 69) {
                             System.out.println((i + 1) + "회차 등급 : D");
-                        } else if (num > 60) {
+                        } else if (num > 59) {
                             System.out.println((i + 1) + "회차 등급 : F");
                         } else if (num > 0) {
                             System.out.println((i + 1) + "회차 등급 : N");
@@ -201,6 +202,77 @@ public class ScoreManagement {
                 }
             }//해당 고유번호를지닌 학생이 없을시
             else {
+                throw new Exception("해당 고유번호를 가진 수강생이 존재하지않습니다");
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    private static void inquireAverageGradeBySubject() {
+        Scanner sc = new Scanner(System.in);
+        String studentId = checkStudentId(); // 관리할 수강생 고유 번호
+        // 기능 구현 (조회할 특정 과목)
+        int totalScore = 0;
+        int totalRound = 0;
+        int averageScore = 0;
+        System.out.println("회차별 등급을 조회합니다...");
+        // 기능 구현
+
+        try {
+            //scoreStore 맵에 입력한 학생고유번호가 존재할경우
+            if (scoreStore.containsKey(studentId)) {
+                System.out.println("평균 점수 조회를 원하는 과목의 고유번호를 입력하세요");
+                //전체보유중인 subject 의 고유번호 전체를 보여줌
+                printSubjectInfoByStudentId(studentId);
+                String inputSubjectId = sc.nextLine();
+                int indicator = Character.getNumericValue(inputSubjectId.charAt(2));
+                //scoreStore안에있는 Map 을 지정해줌
+                Map<String, Score> innerScoreStore = scoreStore.get(studentId);
+                int[] d = innerScoreStore.get(inputSubjectId).getScores();
+                //평균점수를 위한 총 점수 및 총횟수를 구해줌
+                for (int i = 0; i < d.length; i++) {
+                    if (d[i] > 0) {
+                        totalScore += d[i];
+                        totalRound++;
+                    } else {
+                        continue;
+                    }
+                }
+                //평균점수를 구해줌
+                averageScore = totalScore / totalRound;
+                //평균점수를 기반으로한 등급을 구해줌
+                if (innerScoreStore.containsKey(inputSubjectId) && indicator < 6) {
+                    if (averageScore > 94) {
+                        System.out.println("평균 등급: A");
+                    } else if (averageScore > 89) {
+                        System.out.println("평균 등급: B");
+                    } else if (averageScore > 79) {
+                        System.out.println("평균 등급 : C");
+                    } else if (averageScore > 69) {
+                        System.out.println("평균 등급 : D");
+                    } else if (averageScore > 59) {
+                        System.out.println("평균 등급 : F");
+                    } else if (averageScore > 0) {
+                        System.out.println("평균 등급 : N");
+                    }
+                } else if (innerScoreStore.containsKey(inputSubjectId) && indicator > 5) {
+                    if (averageScore > 89) {
+                        System.out.println("평균 등급: A");
+                    } else if (averageScore > 79) {
+                        System.out.println("평균 등급: B");
+                    } else if (averageScore > 69) {
+                        System.out.println("평균등급 : C");
+                    } else if (averageScore > 59) {
+                        System.out.println("평균 등급 : D");
+                    } else if (averageScore > 49) {
+                        System.out.println("평균 등급 : F");
+                    } else if (averageScore > 0) {
+                        System.out.println("평균 등급 : N");
+                    }
+                }
+                System.out.println("\n등급 조회 성공!");
+            } else {
                 throw new Exception("해당 고유번호를 가진 수강생이 존재하지않습니다");
             }
         } catch (Exception e) {
