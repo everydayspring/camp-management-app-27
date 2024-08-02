b3542e31231cc5b22004139a3fe8364faa5adf47
Changmin Kim <ksjchm4@gmail.com>
2024-08-02 11:17:17 +0900

    getStudentId 예외처리 추가

--- src/camp/CampManagementApplication.java
+++ src/camp/CampManagementApplication.java
@@ -6,15 +6,6 @@ import camp.model.Subject;
 
 import java.util.*;
 
-
-/**
- * Notification
- * Java, 객체지향이 아직 익숙하지 않은 분들은 위한 소스코드 틀입니다.
- * main 메서드를 실행하면 프로그램이 실행됩니다.
- * model 의 클래스들과 아래 (// 기능 구현...) 주석 부분을 완성해주세요!
- * 프로젝트 구조를 변경하거나 기능을 추가해도 괜찮습니다!
- * 구현에 도움을 주기위한 Base 프로젝트입니다. 자유롭게 이용해주세요!
- */
 public class CampManagementApplication {
     // 데이터 저장소
     private static Map<String, Student> studentStore;
@@ -285,7 +276,11 @@ public class CampManagementApplication {
     private static String getStudentId() {
         printStudentInfo();
         System.out.print("\n관리할 수강생의 번호를 입력하시오...");
-        return sc.next();
+        String useName = sc.next();
+        if(!studentStore.containsKey(useName))
+            throw new IllegalArgumentException("해당 학생은 존재하지 않습니다.");
+
+        return useName;
     }
 
     private static String getSubjectIdByStudent(String studentId) {
